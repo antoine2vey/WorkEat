7a4615afe0806c630c08fead2b399292966855af
Antoine <antoine@ewill.fr>
2016-12-12 19:10:05 +0100

    Staging for clarity

--- src/server/models/product.model.js
+++ src/server/models/product.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
-    file: {type: String, required:true},
-    title: {type: String, required:true},
-    description: {type: String, required:true},
-    preparation: String,
-    ingredients: {type: String, required:true},
-    allergics: Array,
-    price: {type: Number, required:true},
-    tag: Array,
-    type: {type: Array, required:true},
-    stars: {type: Number, default: 0}
+  file: {type: String, required:true},
+  title: {type: String, required:true},
+  description: {type: String, required:true},
+  preparation: String,
+  ingredients: {type: String, required:true},
+  allergics: Array,
+  price: {type: Number, required:true},
+  tag: Array,
+  type: {type: Array, required:true},
+  starsNumber: {type: Number, default: 0},
+  votesNumber: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Product', productSchema);
