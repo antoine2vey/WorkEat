f6350bc8ed8abda2ad4ca2c4944d50ce2355eb1c
antoine2vey <antoine.2vey@gmail.com>
2017-01-22 15:12:54 +0100

    Magic delete on bundle choices

--- src/components/bundle/index.js
+++ src/components/bundle/index.js
@@ -67,27 +67,60 @@ export default ['$http', function($http) {
     })
   };
 
-  vm.checkDuplicates = {
-    value: {},
-    entree: (entree) => {
-      let found = false;
-      vm.dessert.forEach((item, index) => {
-        if(entree.name === item.name) {
-          vm.checkDuplicates.value = item;
-          vm.dessert.splice(index, 1);
-          found = true;
-        }
-      });
-      if(!found) {
-        vm.dessert.push(vm.checkDuplicates.value);
+  /**
+   * Supprime les dupliqué dans le tableau ciblé
+   * @param  {Boolean} constant  Type de plat
+   * @param  {Array} compareTo Tableau ciblé à comparer
+   * @param  {Object} object    Object à comparer
+   */
+  function magicDelete(constant, compareTo, object) {
+    // Pour chaque plat :
+    compareTo.forEach((item, i) => {
+      // On vérifie si le nom de l'entree choisie
+      // est présente dans les plats
+      if (object.name === item.name) {
+        // L'item temporaire est stocké
+        tmp = item;
+        // On a donc matché
+        matched = true;
+        // Et l'origin est donc l'entrée
+        origin = constant;
+        // On supprime donc l'entrée choisi des plats (car dupliqué)
+        compareTo.splice(i, 1);
       }
-      console.log(vm.checkDuplicates.value);
+    })
+    // Hors boucle, on vérifie que le nom choisi est différent du tmp
+    // Si on a matché et que l'origin est la même alors :
+    if(object.name !== tmp.name && matched && (constant === origin)) {
+      // On repush le tmp dans le tableau des plats (car il est dispo)
+      compareTo.push(tmp);
+      // On ne match plus
+      matched = false;
+      // L'origin est reset
+      origin = null;
+      // ainsi que le tmp!
+      tmp = {};
+    }
+  }
+
+  let tmp = {};
+  let matched = false;
+  let origin;
+  vm.checkDuplicates = {
+    entree: (entree) => {
+      let isEntree = 'entree';
+      magicDelete(isEntree, vm.plat, entree);
+      magicDelete(isEntree, vm.dessert, entree);
     },
     plat: (plat) => {
-      console.log(plat);
+      let isPlat = 'plat';
+      magicDelete(isPlat, vm.entree, plat);
+      magicDelete(isPlat, vm.dessert, plat);
     },
     dessert: (dessert) => {
-      console.log(dessert);
+      let isDessert = 'dessert';
+      magicDelete(isDessert, vm.entree, dessert);
+      magicDelete(isDessert, vm.plat, dessert);
     }
   }
 }];
