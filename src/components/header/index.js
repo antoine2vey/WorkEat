a3eddfc1407d2fadb9d4944e0b6da9937fd9c4fe
antoine2vey <antoine.2vey@gmail.com>
2017-02-28 21:56:50 +0100

    TEMP : clock

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,15 @@
-export default ['$localStorage', '$state', '$http', function ($localStorage, $state, $http) {
+export default ['$localStorage', '$state', '$http', '$timeout', '$rootScope', function ($localStorage, $state, $http, $timeout, $rootScope) {
   const vm = this;
+  console.log($rootScope)
   vm.user = $localStorage;
+
+  vm.clock = "loading clock...";
+  vm.tickInterval = 1000;
+  const tick = function() {
+    vm.clock = Date.now();
+      $timeout(tick, vm.tickInterval);
+    };
+  $timeout(tick, vm.tickInterval);
 
   vm.logout = () => {
     $http.get('/account/logout').success(() => {
