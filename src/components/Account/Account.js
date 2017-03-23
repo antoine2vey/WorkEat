cb2aec23714358251fca4cc96f400d2c8e584334
antoine2vey <antoine.2vey@gmail.com>
2017-03-23 09:16:38 +0100

    JWT + login logic

---
+++ src/components/Account/Account.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Account.css';
+
+class Account extends Component {
+  render() {
+    return (
+      <div>
+        LUL
+      </div>
+    );
+  }
+}
+
+export default Account;
+
