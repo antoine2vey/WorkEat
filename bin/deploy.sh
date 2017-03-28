2a82d70f0269df7a6588da306ee6abb68cc1e3dc
antoine2vey <antoine.2vey@gmail.com>
2017-03-28 23:06:10 +0200

    testing deploy2

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,18 +2,12 @@
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
   echo "Clears git information"
   rm -rf .git
-  echo "Rebuilds dependencies"
-  rm -rf vendor
-  composer install --no-dev --no-interaction --ignore-platform-reqs
   echo "Writing custom gitignore for build"
-  echo "# Build Ignores" > .gitignore
-  echo "composer.phar" >> .gitignore
-  echo "config.json" >> .gitignore
+  echo "node_modules/" >> .gitignore
   echo "deploy_key.*" >> .gitignore
-  echo "build/" >> .gitignore
-  echo "codeclimate.json" >> .gitignore
-  echo "coverage.xml" >> .gitignore
+  cat .gitignore
   echo "Sets up package for sending"
+  
   git init
   git remote add deploy $DEPLOY_URI
   git config user.name $DEPLOY_USER
@@ -22,10 +16,10 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]
   git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
   echo "Sets up permissions"
   echo -e "Host comments.reynrick.com\n\tStrictHostKeyChecking no" >> ~/.ssh/config
-  openssl aes-256-cbc -K $encrypted_a9d53792e855_key -iv $encrypted_a9d53792e855_iv -in deploy_key.pem.enc -out deploy_key.pem -d
+  openssl aes-256-cbc -K $encrypted_a9d53792e855_key -iv $encrypted_a9d53792e855_iv -in deploy-key.enc -out deploy-key.pem -d
   eval "$(ssh-agent -s)"
-  chmod 600 deploy_key.pem
-  ssh-add deploy_key.pem
+  chmod 600 deploy-key.pem
+  ssh-add deploy-key.pem
   echo "Sends build"
   git push -f deploy master
 fi
