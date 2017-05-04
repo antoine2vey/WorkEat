9b6a07b0141125e5260e257c91a01426eada6063
Pierre Clouet <maxclouet@wanadoo.fr>
2017-05-04 21:39:43 +0200

    Fix quasi global du footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,52 +10,54 @@ class About extends Component {
   render() {
     return (
       <div>
-        <div className="container-fluid">
-          <div className="header">
-            <h1 className="header-title">A propos</h1>
-          </div>
-        </div>
-        <div className="container-fluid">
-          <div className="explain">
-            <div className="explain-content">
-              <h1 className="explain-content-title">Nous, WorkEat</h1>
-              <hr className="divider" />
-              <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
-            </div>
-            <div className="explain-image explain-image--concept" />
-          </div>
-        </div>
-        <div className="container-fluid">
-          <div className="explain">
-            <div className="explain-image explain-image--produits" />
-            <div className="explain-content">
-              <h1 className="explain-content-title">Des produits de qualité</h1>
-              <hr className="divider" />
-              <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+        <div className="about">
+          <div className="container-fluid">
+            <div className="header">
+              <h1 className="header-title">A propos</h1>
             </div>
           </div>
-        </div>
-        <div className="container-fluid">
-          <div className="explain">
-            <div className="explain-content">
-              <h1 className="explain-content-title">Gastronomique et économique</h1>
-              <hr className="divider" />
-              <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+          <div className="container-fluid">
+            <div className="explain">
+              <div className="explain-content">
+                <h1 className="explain-content-title">Nous, WorkEat</h1>
+                <hr className="divider" />
+                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
+                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+              </div>
+              <div className="explain-image explain-image--concept" />
             </div>
-            <div className="explain-image explain-image--gastronomie" />
           </div>
-        </div>
-        <div className="container-fluid">
-          <div className="explain">
-            <div className="explain-image explain-image--livraison" />
-            <div className="explain-content">
-              <h1 className="explain-content-title">Livré où vous voulez</h1>
-              <hr className="divider" />
-              <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+          <div className="container-fluid">
+            <div className="explain">
+              <div className="explain-image explain-image--produits" />
+              <div className="explain-content">
+                <h1 className="explain-content-title">Des produits de qualité</h1>
+                <hr className="divider" />
+                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
+                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid">
+            <div className="explain">
+              <div className="explain-content">
+                <h1 className="explain-content-title">Gastronomique et économique</h1>
+                <hr className="divider" />
+                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
+                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+              </div>
+              <div className="explain-image explain-image--gastronomie" />
+            </div>
+          </div>
+          <div className="container-fluid">
+            <div className="explain">
+              <div className="explain-image explain-image--livraison" />
+              <div className="explain-content">
+                <h1 className="explain-content-title">Livré où vous voulez</h1>
+                <hr className="divider" />
+                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
+                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+              </div>
             </div>
           </div>
         </div>
