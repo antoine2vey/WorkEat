5b05997957c3ab51db278de9aca0ad1f4f9673aa
Pierre Clouet <maxclouet@wanadoo.fr>
2017-05-04 21:22:51 +0200

    Fix footer + tabs account + card

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -8,23 +8,23 @@ import Solde from './Solde';
 class Account extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="compteInfo">
-          <div className="container-fluid">
-            <div className="compteInfo-submenu">
-              <NavLink to="/compte/infos" className="compteInfo-submenu-item">Informations Générales</NavLink>
-              <NavLink to="/compte/commandes" className="compteInfo-submenu-item">Commandes</NavLink>
-              <NavLink to="/compte/solde" className="compteInfo-submenu-item">Mon Solde</NavLink>
-            </div>
+      <div>
+      <div className="tabs-container">
+        <div className="container-fluid">
+          <div className="compteInfo-submenu">
+            <NavLink to="/compte/infos" className="compteInfo-submenu-item select-tab">Informations Générales</NavLink>
+            <NavLink to="/compte/commandes" className="compteInfo-submenu-item select-tab">Commandes</NavLink>
+            <NavLink to="/compte/solde" className="compteInfo-submenu-item select-tab">Mon Solde</NavLink>
           </div>
-
+        </div>
+      </div>
+        <div className="compteInfo">
           <Switch>
               <Route path="/compte/infos" component={InfoGenerale} />
               <Route path="/compte/commandes" component={Commandes} />
               <Route path="/compte/solde" component={Solde} />
               <Redirect  from="/compte/" to="/compte/infos" />
           </Switch>
-
         </div>
       </div>
     );
