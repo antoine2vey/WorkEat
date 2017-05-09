5a98d56ebde890725a09a30fdf8a325c28f752a8
antoine2vey <antoine.2vey@gmail.com>
2017-05-09 16:24:53 +0200

    UX improvements + cookie still bugged

--- src/components/Cookie/Cookie.js
+++ src/components/Cookie/Cookie.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 
 class Cookie extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-}
-    cookieValid(){
-      document.cookie += ' cookie=off';
-      var element = document.getElementById("cookie-valider");
-      element.classList.add('fadeOut');
   }
+
+  cookieValid() {
+    document.cookie += ' workEat_cookie=off';
+    const element = document.getElementById('cookie-valider');
+    element.classList.add('fadeOut');
+  }
+
   render() {
-    console.log(document.cookie);
-    console.log(document.cookie.indexOf('cookie'))
     return (
       <div>
-      { (document.cookie.indexOf('cookie') === -1) &&
-          <div className="container-fluid cookie" id="cookie-valider">
-              <p className="cookie-content">En continuant à naviguer sur ce site vous acceptez nos conditions d'utilisations
-                  <button className="cookie-valider" onClick={this.cookieValid} >OK</button>
-                  <a href="https://www.cnil.fr/fr/site-web-cookies-et-autres-traceurs" target="_blank"><button className="cookie-infos">En savoir plus</button></a></p>
-            </div>
-  }
-  </div>
+        { (document.cookie.indexOf('workEat_cookie') === -1) &&
+        <div className="container-fluid cookie" id="cookie-valider">
+          <p className="cookie-content">En continuant à naviguer sur ce site vous acceptez nos conditions d'utilisations
+            <button className="cookie-valider" onClick={this.cookieValid} >OK</button>
+            <a href="https://www.cnil.fr/fr/site-web-cookies-et-autres-traceurs" rel="noopener noreferrer" target="_blank"><button className="cookie-infos">En savoir plus</button></a></p>
+        </div> }
+      </div>
     );
   }
 }
--- src/components/Products/Entrees.js
+++ src/components/Products/Entrees.js
@@ -1,56 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import chunk from 'lodash/chunk';
 import { addToCart } from '../../actions/cart';
 
 const Entree = ({ entree: { file, description, price, tags, types, name }, addToCart, showProduct, isVisible, hideProduct }) => (
-  <div className="products__product">
-    <div className="products__product__options">
-      <button className="products__product__option btn-gold" onClick={isVisible ? hideProduct : showProduct}>Voir</button>
-      <button className="products__product__option btn-gold" onClick={addToCart}>Ajouter</button>
+  <div className="products__product-container">
+    <div className="products__product">
+      <div className="products__product__options">
+        <button className="products__product__option btn-gold" onClick={isVisible ? hideProduct : showProduct}>Voir</button>
+        <button className="products__product__option btn-gold" onClick={addToCart}>Ajouter</button>
+      </div>
+      <img src={file} alt="Produit" className="products__product__image" />
+      <h2 className="products__product__title">{name}</h2>
+      <p className="products__product__tag">{
+        tags.map(tag => (
+          <span key={tag._id}>{tag.name}, </span>
+        ))
+      }</p>
+      <p className="products__product__price">{price}€</p>
     </div>
-    <img src={file} alt="Produit" className="products__product__image" />
-    <h2 className="products__product__title">{name}</h2>
-    <p className="products__product__tag">{
-      tags.map(tag => (
-        <span key={tag._id}>{tag.name}, </span>
-      ))
-    }</p>
-    <p className="products__product__price">{price}€</p>
   </div>
 );
 
 const Entrees = ({ ...props }) => {
   const { entrees, addToCart, showProduct, hideProduct, isVisible } = props;
-  const rows = chunk(entrees, 4);
 
   return (
-    <div>
-      { rows.map((row, i) => (
-        <div
-          className="products__products-list"
-          key={i}
-        >
-          { row.map(entree => (
-            <Entree
-              entree={entree}
-              key={entree._id}
-              addToCart={() => addToCart(entree)}
-              showProduct={() => showProduct(entree)}
-              hideProduct={() => hideProduct()}
-              isVisible={isVisible}
-            />
-          )) }
-        </div>
+    <div className="products__products-list">
+      { entrees.map(entree => (
+        <Entree
+          entree={entree}
+          key={entree._id}
+          addToCart={() => addToCart(entree)}
+          showProduct={() => showProduct(entree)}
+          hideProduct={() => hideProduct(entree)}
+          isVisible={isVisible}
+        />
       )) }
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  addToCart(id) {
-    dispatch(addToCart(id));
-  },
-});
-
-export default connect(() => ({}), mapDispatchToProps)(Entrees);
+export default connect(() => ({}), { addToCart })(Entrees);
