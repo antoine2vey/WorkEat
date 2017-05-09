2727a0f7303d3d27f5139909a641ae21112eccb4
antoine2vey <antoine.2vey@gmail.com>
2017-05-09 09:44:53 +0200

    ??

--- src/components/Payment/PaymentStepTwo.js
+++ src/components/Payment/PaymentStepTwo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getTotalPrice, getProducts } from '../../reducers/cart';
 import { checkoutCart } from '../../actions/cart';
