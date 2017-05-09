576c2b4db1684a9b55fd2a93836a4f78909c97ba
Pierre Clouet <maxclouet@wanadoo.fr>
2017-05-09 23:47:57 +0200

    Debuts formules

--- src/components/Bundles/Bundle.js
+++ src/components/Bundles/Bundle.js
@@ -20,54 +20,131 @@ class Bundle extends React.Component {
                 <p className="formules__title is-4">{ bundle.name }</p>
                 <p className="formule__price is-6">{ bundle.price }€</p>
                 {/* <p className="formule__desc">{ bundle.desc }</p> */}
-
-                <div className="content">
-
-                </div>
+              </div>
+              <div className="formules__choise">
+                { bundle.items.hasEntree &&
+                <div>
+                  <label htmlFor="entree">entree</label>
+                  <div name="entree" id="entree" className="formules__select active">
+                    { products.filter(product => product.types.indexOf('entree') > -1).map(entree => (
+                      <div id={entree._id} className="formules__item">
+                        <div className="formules__item-image">
+                          <img src={entree.file} alt={entree.name} />
+                        </div>
+                        <div className="formules__item-infos">
+                          <h3 className="formules__item-title">{entree.name}</h3>
+                          <p className="formules__item-desc">{
+                            entree.tags.map(tag => (
+                              <span>{tag.name}, </span>
+                            ))
+                          }</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div> }
+                {/* { bundle.items.hasEntree &&
+                <div>
+                  <label htmlFor="entree">entree</label>
+                  <select name="entree" id="entree" className="formules__select">
+                    { products.filter(product => product.types.indexOf('entree') > -1).map(entree => (
+                      <option value={entree._id} key={entree._id}>{entree.name}</option>
+                    ))}
+                  </select>
+                </div> } */}
+                { bundle.items.hasPlat &&
+                <div>
+                  <label htmlFor="plat">plat</label>
+                  <div name="plat" id="plat" className="formules__select active">
+                    { products.filter(product => product.types.indexOf('plat') > -1).map(plat => (
+                      <div id={plat._id} className="formules__item">
+                        <div className="formules__item-image">
+                          <img src={plat.file} alt={plat.name} />
+                        </div>
+                        <div className="formules__item-infos">
+                          <h3 className="formules__item-title">{plat.name}</h3>
+                          <p className="formules__item-desc">{
+                            plat.tags.map(tag => (
+                              <span>{tag.name}, </span>
+                            ))
+                          }</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div> }
+                {/* { bundle.items.hasPlat &&
+                <div>
+                  <label htmlFor="plat">plat</label>
+                  <select name="plat" id="plat">
+                    { products.filter(product => product.types.indexOf('plat') > -1).map(plat => (
+                      <option value={plat._id} key={plat._id}>{plat.name}</option>
+                    ))}
+                  </select>
+                </div> } */}
+                { bundle.items.hasDessert &&
+                <div>
+                  <label htmlFor="dessert">plat</label>
+                  <div name="dessert" id="dessert" className="formules__select active">
+                    { products.filter(product => product.types.indexOf('dessert') > -1).map(dessert => (
+                      <div id={dessert._id} className="formules__item">
+                        <div className="formules__item-image">
+                          <img src={dessert.file} alt={dessert.name} />
+                        </div>
+                        <div className="formules__item-infos">
+                          <h3 className="formules__item-title">{dessert.name}</h3>
+                          <p className="formules__item-desc">{
+                            dessert.tags.map(tag => (
+                              <span>{tag.name}, </span>
+                            ))
+                          }</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div> }
+                {/* { bundle.items.hasDessert &&
+                <div>
+                  <label htmlFor="dessert">dessert</label>
+                  <select name="dessert" id="dessert">
+                    { products.filter(product => product.types.indexOf('dessert') > -1).map(dessert => (
+                      <option value={dessert._id} key={dessert._id}>{dessert.name}</option>
+                    ))}
+                  </select>
+                </div> } */}
+                { bundle.items.hasBoisson &&
+                <div>
+                  <label htmlFor="boisson">plat</label>
+                  <div name="boisson" id="boisson" className="formules__select active">
+                    { products.filter(product => product.types.indexOf('boisson') > -1).map(boisson => (
+                      <div id={boisson._id} className="formules__item">
+                        <div className="formules__item-image">
+                          <img src={boisson.file} alt={boisson.name} />
+                        </div>
+                        <div className="formules__item-infos">
+                          <h3 className="formules__item-title">{boisson.name}</h3>
+                          <p className="formules__item-desc">{
+                            boisson.tags.map(tag => (
+                              <span>{tag.name}, </span>
+                            ))
+                          }</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div> }
+                {/* { bundle.items.hasBoisson &&
+                <div>
+                  <label htmlFor="boisson">boisson</label>
+                  <select name="boisson" id="boisson">
+                    { products.filter(product => product.types.indexOf('boisson') > -1).map(boisson => (
+                      <option value={boisson._id} key={boisson._id}>{boisson.name}</option>
+                    ))}
+                  </select>
+                </div> } */}
               </div>
             </div>
           </div>
-        </div>
-        <div className="content">
-          { bundle.items.hasEntree &&
-          <div>
-            <label htmlFor="entree">entree</label>
-            <select name="entree" id="entree">
-              { products.filter(product => product.types.indexOf('entree') > -1).map(entree => (
-                <option value={entree._id} key={entree._id}>{entree.name}</option>
-              ))}
-            </select>
-          </div> }
-          <br/>
-          { bundle.items.hasPlat &&
-          <div>
-            <label htmlFor="plat">plat</label>
-            <select name="plat" id="plat">
-              { products.filter(product => product.types.indexOf('plat') > -1).map(plat => (
-                <option value={plat._id} key={plat._id}>{plat.name}</option>
-              ))}
-            </select>
-          </div> }
-          <br/> 
-          { bundle.items.hasDessert &&
-          <div>
-            <label htmlFor="dessert">dessert</label>
-            <select name="dessert" id="dessert">
-              { products.filter(product => product.types.indexOf('dessert') > -1).map(dessert => (
-                <option value={dessert._id} key={dessert._id}>{dessert.name}</option>
-              ))}
-            </select>
-          </div> }
-          <br/>
-          { bundle.items.hasBoisson &&
-          <div>
-            <label htmlFor="boisson">boisson</label>
-            <select name="boisson" id="boisson">
-              { products.filter(product => product.types.indexOf('boisson') > -1).map(boisson => (
-                <option value={boisson._id} key={boisson._id}>{boisson.name}</option>
-              ))}
-            </select>
-          </div> }
         </div>
       </div>
     );
