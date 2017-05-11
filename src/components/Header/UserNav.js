3a6e0f99c3d734b693ca5b7c1cd2280b420f646b
antoine2vey <antoine.2vey@gmail.com>
2017-05-12 00:46:37 +0200

    Amélioration du timer

--- src/components/Header/UserNav.js
+++ src/components/Header/UserNav.js
@@ -16,9 +16,17 @@ class UserNav extends Component {
         const tmrw = moment().add(1, 'day').hours('10').minutes('30').seconds('00').format();
         const tmrwTimestamp = moment(tmrw).unix();
 
-        this.setState({ tstamp: moment((tmrwTimestamp - date) * 1000).format('HH:mm:ss') });
-      }),
-      tstamp: moment().format('HH:mm:ss'),
+        const before = moment().hours('7').minutes('00').seconds('00').format();
+        const after = moment().hours('10').minutes('30').seconds('00').format();
+
+        // Si je suis entre 8h et 11h30
+        if (moment().isAfter(before) && moment().isBefore(after)) {
+          this.setState({ tstamp: moment((tmrwTimestamp - date) * 1000).format('HH:mm:ss') });
+        }
+
+        this.setState({ tstamp: 'Reviens à partir de 8h!' });
+      }, 1000),
+      tstamp: '',
     };
 
     this.showCart = this.showCart.bind(this);
