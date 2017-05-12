5d6a8ffade278ab4cd6954d0f57fcdb7d474ebb1
Clem <clementcavigniaux@gmail.com>
2017-05-12 11:25:27 +0200

    test about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,8 +21,7 @@ class About extends Component {
               <div className="explain-content">
                 <h1 className="explain-content-title">Nous, WorkEat</h1>
                 <hr className="divider" />
-                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+                <p className="explain-content-text">Chez WorkEat nous vous proposons chaques jours une large gamme de mets livrés à proximité de votre entreprise. Cela vous permet de déguster le meilleur de la gastronomie tout en restant au travail.  </p>
               </div>
               <div className="explain-image explain-image--concept" />
             </div>
@@ -33,8 +32,7 @@ class About extends Component {
               <div className="explain-content">
                 <h1 className="explain-content-title">Des produits de qualité</h1>
                 <hr className="divider" />
-                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+                <p className="explain-content-text">Nos produits sont soigneusement sélectionnés par nos experts afin de vous proposer des mets de qualités. Nos produits sont achetés au près de producteurs locaux afin de favoriser l'économie local et réduire notre impact environnemental. </p>
               </div>
             </div>
           </div>
@@ -43,8 +41,7 @@ class About extends Component {
               <div className="explain-content">
                 <h1 className="explain-content-title">Gastronomique et économique</h1>
                 <hr className="divider" />
-                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+                <p className="explain-content-text">Nous selectionons nos produits en fonction de leurs qualités gustatives tout en essayant de maintenir nos prix les plus abordable possible.</p>
               </div>
               <div className="explain-image explain-image--gastronomie" />
             </div>
@@ -55,8 +52,7 @@ class About extends Component {
               <div className="explain-content">
                 <h1 className="explain-content-title">Livré où vous voulez</h1>
                 <hr className="divider" />
-                <p className="explain-content-text">Etiam efficitur nunc a diam dignissim pulvinar. Donec mollis enim nunc, vitae consectetur turpis pharetra sed. Maecenas a tellus nec urna ornare molestie in ut justo. Donec congue odio ut velit pellentesque ullamcorper. Curabitur gravida suscipit
-                  eleifend. Duis ac malesuada nunc, eleifend maximus ipsum. Maecenas dictum varius dolor eu lobortis.</p>
+                <p className="explain-content-text">L'ensemble de nos plats sont livrés au point de livraison de votre choix afin d'en profiter où vous le voulez. </p>
               </div>
             </div>
           </div>
