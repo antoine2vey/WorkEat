da1a43bb33e8eafb5f030091d200e885bc629399
shaark <antoine.2vey@gmail.com>
2017-08-04 09:08:53 +0200

    CSV generation is working, checking for duplicates

--- src/components/Prestataire/Prestataire.js
+++ src/components/Prestataire/Prestataire.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Prestataire extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      date: new Date().toISOString(),
+    };
+
+    this.changeDate = this.changeDate.bind(this);
+    this.downloadCSV = this.downloadCSV.bind(this);
+  }
+
+  downloadCSV() {
+    const { date } = this.state;
+
+    axios.post('/api/csv', { date })
+    .then(res => console.log(res))
+    .catch(({ response }) => console.error(response));
+  }
+
+  changeDate(event) {
+    const { valueAsDate } = event.target;
+
+    this.setState({
+      date: valueAsDate.toISOString(),
+    });
+  }
+
   render() {
     return (
-      <div>
-        presta
+      <div style={{ paddingTop: 100 }}>
+        <input type="date" onChange={this.changeDate} />
+        <button onClick={this.downloadCSV}>downloadCSV</button>
       </div>
     );
   }
