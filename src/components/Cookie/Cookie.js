4724bf60a64f7bc749c30c04d44a033cb0ab0d3a
Clem <clementcavigniaux@gmail.com>
2017-08-28 14:15:32 +0200

    Coockie fix + css

--- src/components/Cookie/Cookie.js
+++ src/components/Cookie/Cookie.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Cookie extends Component {
   cookieValid() {
-    document.cookie += ' workEat_cookie=1';
+    document.cookie = ' workEat_cookie=off';
     const element = document.getElementById('cookie-valider');
     element.classList.add('fadeOut');
   }
